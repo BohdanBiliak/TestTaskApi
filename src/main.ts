13ebc0fde75da00264ac6c45ede90028b9a15768
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2026-02-05 12:47:33 +0100

    add global exception filter for centralized error handling

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { config } from './config/config';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.port);
 }
