39b1477f3b5dce64b99309e5e0bef1dc352bf6e9
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2026-02-05 10:18:47 +0100

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from './config/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongodb.uri)],
+  imports: [MongooseModule.forRoot(config.mongodb.uri), UsersModule],
   controllers: [],
   providers: [],
 })
